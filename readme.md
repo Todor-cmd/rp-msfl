340f9291aa767bbd805e4b6c1dd8c36de69e569a
Todor-cmd <79723091+Todor-cmd@users.noreply.github.com>
2024-01-30 12:47:32 +0100

    Better Organisation

--- readme.md
+++ readme.md
@@ -1,7 +1,28 @@
-https://github.com/vrt1shjwlkr/NDSS21-Model-Poisoning/tree/main/cifar10
-modification of this to work in a FedMes environment
+<h1>Robustness Against Untargeted Attacks of Multi-Server Federated Learning for
+Image Classification</h1>
 
-For first time run
+This repository provides extensions of state-of-art defenses Median, Krum, Multi-Krum, Bulyan, Trimmed-Mean and DnC to Multi-Server Federated Learning with FedMes. As-well as the Min-Max attack.
+
+Along with the data collected in results folder, the code should allow for transparent evaluation and reproductions of the findings in our paper *Robustness Against Untargeted Attacks of Multi-Server Federated Learning for Image Classification: Are Defenses Based on Existing Methods Enough?* 
+
+This work was done as part of the 2023-2024 Q2 Reseach Project at TU Delft. To see the full works done as part of this project click [here](https://cse3000-research-project.github.io/).
+
+
+Repository inspired by:  
+https://github.com/vrt1shjwlkr/NDSS21-Model-Poisoning  
+https://github.com/GillHuang-Xtler/msfl
+
+In collaboration with:  
+https://github.com/Riliano/rp-msfl
+
+
+To set-up repository run:
+```
+pip install -r pip_requirements.txt
+```
+For first time, to download data-sets, run:
 ```
 python data.py
 ```
+
+Then arguments.py can be set up for the required setting and main.py can be run. 
--- utils/plot_results.py
+++ utils/plot_results.py
@@ -1,30 +1,97 @@
+import os
+import numpy as np
 import seaborn as sns;
 import pandas as pd;
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('results/alexnet-cifar10/attack-case-1/170124-1806-cifar10-attack case 1-Fedmes-1500e-2att-min-max-alexnet.csv')
-df1 = pd.read_csv('results/alexnet-cifar10/attack-case-2/170124-1820-cifar10-attack case 2-Fedmes-1500e-2att-min-max-alexnet.csv')
-df2 = pd.read_csv('results/alexnet-cifar10/no-attack/170124-1424-cifar10-multi-cross-Fedmes-1500e-0att-min-max-alexnet.csv')
+cases = ['no-attack', 'attack-case-1', 'attack-case-2', 'fmes-median', 'fmes-krum-2', 'fmes-m-krum', 'fmes-trimmed-mean', 'fmes-bulyan' ,'fmes-dnc-2']
+
+# for case in cases:
+#     directory = 'results/alexnet-fashion/' + case + '/'
+
+#     # List all files in the directory
+#     all_files = os.listdir(directory)
+
+#     # Filter only CSV files
+#     csv_files = [file for file in all_files if file.endswith('.csv')]
+
+#     # Sort the CSV files alphabetically (to ensure consistent order)
+#     csv_files.sort()
+
+#     # Assign the first three CSV files to df, df1, and df2 respectively
+#     df = pd.read_csv(os.path.join(directory, csv_files[0]))
+#     df1 = pd.read_csv(os.path.join(directory, csv_files[1]))
+#     df2 = pd.read_csv(os.path.join(directory, csv_files[2]))
+
+#     df_te_acc = df['Final Test Accuracy'].iloc[-1]
+#     df1_te_acc = df1['Final Test Accuracy'].iloc[-1]
+#     df2_te_acc = df2['Final Test Accuracy'].iloc[-1]
+
+    
+#     print(case, ': ',df_te_acc, '| ', df1_te_acc, '| ', df2_te_acc)
+
+#     numbers = [df_te_acc, df1_te_acc, df2_te_acc]
+
+    
+directory = 'results/alexnet-fashion/' + cases[0] + '/'
+all_files = os.listdir(directory)
+csv_files1 = [file for file in all_files if file.endswith('.csv')]
+csv_files1.sort()
+    
+directory2 = 'results/alexnet-fashion/' + cases[1] + '/'
+all_files2 = os.listdir(directory2)
+csv_files2 = [file for file in all_files2 if file.endswith('.csv')]
+csv_files2.sort()
+
+directory3 = 'results/alexnet-fashion/' + cases[2] + '/'
+all_files3 = os.listdir(directory3)
+csv_files3 = [file for file in all_files3 if file.endswith('.csv')]
+csv_files3.sort()
 
 
+df1 = pd.read_csv(os.path.join(directory, csv_files1[1]))
+df2 = pd.read_csv(os.path.join(directory2, csv_files2[1]))
+df3 = pd.read_csv(os.path.join(directory3, csv_files3[0]))
 
 # Calculate rolling mean with a window of 30
-df['Rolling_Mean'] = df['Accuracy'].rolling(window=30).mean()
-df1['Rolling_Mean'] = df1['Accuracy'].rolling(window=30).mean()
-df2['Rolling_Mean'] = df2['Accuracy'].rolling(window=30).mean()
+df1['Rolling_Mean'] = df1['Validation Accuracy'].rolling(window=30).mean()
+df2['Rolling_Mean'] = df2['Validation Accuracy'].rolling(window=30).mean()
+df3['Rolling_Mean'] = df3['Validation Accuracy'].rolling(window=30).mean()
 
 # Plot the accuracy using Seaborn
-sns.lineplot(x=df.index, y='Rolling_Mean', data=df, label='Attack Case 1')
-sns.lineplot(x=df1.index, y='Rolling_Mean', data=df1, label='Attack Case 2')
-sns.lineplot(x=df2.index, y='Rolling_Mean', data=df2, label='No Attack')
+sns.set_theme()
+sns.lineplot(x=df1.index, y='Rolling_Mean', data=df1, label='No Attack')
+sns.lineplot(x=df2.index, y='Rolling_Mean', data=df2, label='Attack Case 1')
+sns.lineplot(x=df3.index, y='Rolling_Mean', data=df3, label='Attack Case 2')
 
 # Label the axes
 plt.xlabel('Epoch')
-plt.ylabel('Accuracy')
-plt.title('Cifar10 with AlexNet - Rolling Mean (Window=30)')
+plt.ylabel('Validation Accuracy')
+plt.title('Fashion-MNIST with AlexNet - Rolling Mean (Window=30)')
 
 # Show the plot
 plt.show()
+
+# # Calculate rolling mean with a window of 30
+# df1= df1['Validation Accuracy']
+# df2= df2['Validation Accuracy']
+# df3= df3['Validation Accuracy']
+
+# # Plot the accuracy using Seaborn
+# sns.set_theme()
+# sns.lineplot(x=df1.index, y = df1, data=df1, label='No Defense')
+# sns.lineplot(x=df2.index, y = df2, data=df2, label='Attack Case 1')
+# sns.lineplot(x=df3.index, y = df3, data=df3, label='Attack Case 2')
+
+
+
+# # Label the axes
+# plt.xlabel('Epoch')
+# plt.ylabel('Validation Accuracy')
+# plt.title('Fashion-MNIST with VGG11')
+
+# # Show the plot
+# plt.show()
 
 
 
